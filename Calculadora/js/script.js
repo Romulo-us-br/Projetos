e213fb22c799145ebcf61985b5c14c452d26c9e9
Romulus <romulop.brasil25@gmail.com>
2025-02-13 00:38:59 -0300

    add defer

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -2,10 +2,11 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
-multiplicationForm.addEventListener("submit", (e)=>{
+multiplicationForm.addEventListener("submit", (e)=> {
     e.preventDefault();
     const multiplicationNumber = +numberInput.value;
     const multiplicatorNumber = +multiplicationInput.value;
 
-    console.log(multiplicationNumber, multiplicatorNumber)
-})
+    
+    console.log(multiplicationNumber, multiplicatorNumber);
+});
